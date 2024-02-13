852eaa2de561da301cd89ac56da6e11e0a99ecd9
Nikita Peplov <npeplov@yandex.ru>
2024-02-13 11:55:04 +0300

    form.tsx error

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,20 @@
-const OrganizationPage = () => {
-  return (
-    <div>
-      Organization Page
-     </div>
-  )
-}
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
 
-export default OrganizationPage
+const OrganizationPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationPage;
