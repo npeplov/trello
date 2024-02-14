cb600680c50c425a98c1a03b67e472f640ee33e4
Nikita Peplov <npeplov@yandex.ru>
2024-02-14 15:13:42 +0300

    showing images

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,10 @@
+import {z} from "zod";
+
+export const CreateBoard = z.object({
+  title: z.string({
+    required_error: "Title is requred",
+    invalid_type_error: "Title is requred",
+  }).min(3, {
+    message: "Title is too short"
+  })
+})
